4fd608496578b603b7402cbf04b4667eda2018d7
Nathan Castle <nathan.castle@outlook.com>
2015-08-09 13:11:30 -0400

    Added reactivity and filtering

--- client/controllers/dashboard/dashboard.js
+++ client/controllers/dashboard/dashboard.js
@@ -2,3 +2,19 @@
 UI.registerHelper("eq", function(one, two){
     return one == two;
 });
+
+Template.dashboard.events({
+  'click #Email' : function(){
+    Session.set('filter', { type: 'email' });
+  },
+  'click #Phone' : function(){
+    Session.set('filter', { type: 'phone' });
+  },
+  'click #Text' : function(){
+    Session.set('filter', { type: 'text' });
+  },
+  'click #All' : function(){
+    Session.set('filter', {});
+  },
+
+});
