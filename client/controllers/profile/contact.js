bca431f03940fe2fe7c44c363335cddda16a890b
James Michael Reilly <jmr8893@rit.edu>
2015-08-09 10:06:18 -0400

    collections stuff

--- client/controllers/profile/contact.js
+++ client/controllers/profile/contact.js
@@ -1,11 +1,17 @@
-var postHooks = {
-  before: {
-    insert: function(doc) {
-      if(Meteor.userId()){
-        console.log("HOOKED");
-        doc.userId = Meteor.userId();
-        return doc;
+
+
+
+Template.contact.rendered = function() {
+  var postHooks = {
+    before: {
+      insert: function(doc) {
+        if(Meteor.userId()){
+          console.log("HOOKED");
+          doc.userId = Meteor.userId();
+          return doc;
+        }
       }
     }
   }
+  AutoForm.addHooks('insertPostForm', postHooks);
 }
